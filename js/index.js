0e14ec82f1cfccda23602c5e6117925a4efbf26f
natalietate <martin.nat@gmail.com>
2017-11-25 19:09:56 -0500

    query selectors and variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
-console.log("hello world")
+window.addEventListener('load', function() {
+  var elements = {
+    txtMarble: document.querySelector("#marbles"),
+
+    txtMosaicEndless: document.querySelector("#endless-sky"),
+    btnMosaicEndless: document.querySelector("#build-endless-sky"),
+
+    txtMosaicUndersea: document.querySelector("#undersea"),
+    btnMosaicUndersea: document.querySelector("#build-undersea"),
+
+    txtMosaicSequined: document.querySelector("#sequined"),
+    btnMosaicSequined: document.querySelector("#build-sequined"),
+
+    txtMosaicGlow: document.querySelector("#glow-dark"),
+    btnMosaicGlow: document.querySelector("#build-glow-dark")
+  };
+
+  var numMarble = 0;
+  var mosaicEndless = {
+    count: 0,
+    cost: 10,
+    marble: 1
+  };
+  var mosaicUndersea = {
+    count: 0,
+    cost: 250,
+    marble: 10
+  };
+  var mosaicSequined = {
+    count: 0,
+    cost: 500,
+    marble: 25
+  };
+  var mosaicGlow = {
+    count: 0,
+    cost: 1000,
+    marble: 50
+  };
+
+});
