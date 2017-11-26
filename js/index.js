02f1f72d70b526f175f50c7c01b2c7f6e78281fe
natalietate <martin.nat@gmail.com>
2017-11-25 19:24:57 -0500

    gameplay

--- js/index.js
+++ js/index.js
@@ -11,8 +11,8 @@ window.addEventListener('load', function() {
     txtMosaicSequined: document.querySelector("#sequined"),
     btnMosaicSequined: document.querySelector("#build-sequined"),
 
-    txtMosaicGlow: document.querySelector("#glow-dark"),
-    btnMosaicGlow: document.querySelector("#build-glow-dark")
+    txtMosaicGlow: document.querySelector("#glow"),
+    btnMosaicGlow: document.querySelector("#build-glow")
   };
 
   var numMarble = 0;
@@ -36,5 +36,85 @@ window.addEventListener('load', function() {
     cost: 1000,
     marble: 50
   };
+
+  document.querySelector("#gather-marble").addEventListener('click', function(evt) {
+    numMarble++;
+    updateMarbleText();
+  });
+
+  document.querySelector("#build-endless-sky").addEventListener('click', function(evt) {
+    if (numMarble < mosaicEndless.cost) return;
+
+    mosaicEndless.count += 1;
+    numMarble -= mosaicEndless.cost;
+    mosaicEndless.cost = Math.ceil(mosaicEndless.cost * 1.25);
+    updateEndlessSkyText();
+    updateMarbleText();
+  });
+
+  document.querySelector("#build-undersea").addEventListener('click', function(evt) {
+    if (numMarble < mosaicUndersea.cost) return;
+
+    mosaicUndersea.count += 1;
+    numMarble -= mosaicUndersea.cost;
+    mosaicUndersea.cost = Math.ceil(mosaicUndersea.cost * 1.25);
+    updateUnderseaText();
+    updateMarbleText();
+  });
+
+  document.querySelector("#build-sequined").addEventListener('click', function(evt) {
+    if (numMarble < mosaicSequined.cost) return;
+
+    mosaicSequined.count += 1;
+    numMarble -= mosaicSequined.cost;
+    mosaicSequined.cost = Math.ceil(mosaicSequined.cost * 1.5);
+    updateSequinedText();
+    updateMarbleText();
+  });
+
+  document.querySelector("#build-glow").addEventListener('click', function(evt) {
+    if (numMarble < mosaicGlow.cost) return;
+
+    mosaicGlow.count += 1;
+    numMarble -= mosaicGlow.cost;
+    mosaicGlow.cost = Math.ceil(mosaicGlow.cost * 2);
+    updateGlowText();
+    updateMarbleText();
+  });
+
+  function updateMarbleText() {
+    elements.txtMarble.innerHTML = numMarble;
+  }
+
+  function updateEndlessSkyText() {
+    elements.txtMosaicEndless.innerHTML = mosaicEndless.count;
+    elements.btnMosaicEndless.innerHTML = "Build (" + mosaicEndless.cost + ")";
+  }
+
+  function updateUnderseaText() {
+    elements.txtMosaicUndersea.innerHTML = mosaicUndersea.count;
+    elements.btnMosaicUndersea.innerHTML = "Build (" + mosaicUndersea.cost + ")";
+  }
+
+  function updateSequinedText() {
+    elements.txtMosaicSequined.innerHTML = mosaicSequined.count;
+    elements.btnMosaicSequined.innerHTML = "Build (" + mosaicSequined.cost + ")";
+  }
+
+  function updateGlowText() {
+    elements.txtMosaicGlow.innerHTML = mosaicGlow.count;
+    elements.btnMosaicGlow.innerHTML = "Build (" + mosaicGlow.cost + ")";
+  }
+
+  function gameLoop() {
+    numMarble += Math.ceil(mosaicEndless.count * mosaicEndless.marble);
+    numMarble += Math.ceil(mosaicUndersea.count * mosaicUndersea.marble);
+    numMarble += Math.ceil(mosaicSequined.count * mosaicSequined.marble);
+    numMarble += Math.ceil(mosaicGlow.count * mosaicGlow.marble);
+    updateMarbleText();
+    window.setTimeout(gameLoop, 1000);
+  }
+
+  gameLoop();
 
 });
